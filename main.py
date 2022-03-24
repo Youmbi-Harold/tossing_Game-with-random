50c6c07d182050cd0220392fe5b2f85f0dbc6993
YOUMBI Harold <80352823+Youmbi-Harold@users.noreply.github.com>
2022-03-24 21:35:45 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+import random #importing package
+
+toss = random.randint(0,1) #establishing a range of from 0 to 1
+if toss == 1: #verifying if it is head or tail
+  print("Heads")
+else :
+  print("Tails")
+
+
+
+
+
+
+
+
+
